259e7219bf719edeece7f7360e6973b41524210f
RealSeaberry <128274146+RealSeaberry@users.noreply.github.com>
2024-05-08 23:36:42 +0800

    Update match_shuowen_multi.py

--- script/match_shuowen_multi.py
+++ script/match_shuowen_multi.py
@@ -3,11 +3,11 @@ from match_shuowen_part import match_shuowen_part
 
 def match_shuowen_multi(string,type,shuowen_loc,number):
     
-    # 将任务分配给10个线程
-    pool = multiprocessing.Pool(10)
+    # 将任务分配给number个线程
+    pool = multiprocessing.Pool(number)
     results = []
 
-    # 将json文件分成10个部分，每个部分分配给一个线程
+    # 将json文件分成number个部分，每个部分分配给一个线程
     for i in range(number):
         start_index = i * 9833 // number + 1
         end_index = (i + 1) * 9833 // number
